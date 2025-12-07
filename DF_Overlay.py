b5c57e5fc4b7182f2141d5878bc0e9e96627f3af
auto builder <actions@github.com>
2025-12-07 23:13:22 +0000

    Auto bump version to 1.76

--- DF_Overlay.py
+++ DF_Overlay.py
@@ -32,7 +32,7 @@ settings_win = None
 
 session = requests.Session()
 session.headers.update({"User-Agent": "Mozilla/5.0"})
-GITHUB_VERSION_PUSH = "1.75"
+GITHUB_VERSION_PUSH = "1.76"
 CURRENT_VERSION = "1.73"
 
 # Fetch patch notes
