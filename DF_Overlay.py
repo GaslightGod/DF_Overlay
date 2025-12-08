67478cd97b1a1df55b73abcc0c6cf0da129c7f93
GaslightGod <GaslightGod@gmail.com>
2025-12-08 14:04:23 -0600

    Added Thread locks, PID switcher, GUI positional permenance

--- DF_Overlay.py
+++ DF_Overlay.py
@@ -38,7 +38,7 @@ settings_win = None
 session = requests.Session()
 session.headers.update({"User-Agent": "Mozilla/5.0"})
 GITHUB_VERSION_PUSH = "1.75"
-CURRENT_VERSION = "1.75"
+CURRENT_VERSION = "1.76"
 
 # Fetch patch notes
 def fetch_patch_notes():
